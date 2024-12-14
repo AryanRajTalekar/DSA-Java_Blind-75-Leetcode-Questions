3276dc8c2969b84d409d5eba087530a2aed43cd6
AryanRajTalekar <aryanrajtalekar@gmail.com>
2024-12-14 19:08:39 +0530

    two sum problem

--- TwoSumProblem.java
+++ TwoSumProblem.java
@@ -1,1 +1,29 @@
+class TwoSumProblem {
+    static boolean test(int[] arr, int n, int target) {
+        int left = 0;
+        int right = n - 1;
 
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                return true; // Pair found
+            } else if (sum < target) {
+                left++; 
+            } else {
+                right--; 
+            }
+        }
+
+        return false; 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 5, 6, 7, 8};
+        int n = arr.length;
+        int target = 9;
+
+        boolean result = test(arr, n, target);
+        System.out.println("Pair found: " + result);
+    }
+}
